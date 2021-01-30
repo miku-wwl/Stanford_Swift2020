1ce591bd2d18f1c4758ecfe9ee9df478147bc878
miku-wwl <38804125+miku-wwl@users.noreply.github.com>
2021-01-29 21:23:02 -0800

    lecture3 updated based on lecture2

---
+++ lecture4/lecture1/lecture1/EmojiMemoryGame.swift
@@ -0,0 +1,38 @@
+//
+//  EmojiMemoryGame.swift
+//  lecture1
+//
+//  Created by Weilai Wang on 28/1/21.
+//  Copyright © 2021 Weilai Wang. All rights reserved.
+//
+
+import SwiftUI
+
+
+
+
+class EmojiMemoryGame: ObservableObject {
+    
+    @Published private var model:MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis: Array<String> = ["👻","🎃","🕷"]
+        return  MemoryGame<String>(numberOfPairsOfCards: emojis.count) { pairIndex in
+            return emojis[pairIndex]
+            
+        }
+    }
+    
+    
+    // mark: - access to the model
+    var cards: Array<MemoryGame<String>.Card>{
+        model.cards
+    }
+    
+    //mark: -intent
+    func choose(card: MemoryGame<String>.Card){
+   //     objectWillChange.send()
+        
+        model.choose(card: card)
+    }
+}
